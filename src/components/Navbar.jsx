9d53fd6d76aba4e4cf1ab7c592316d1b32a0f0eb
blue cat <01claudia.parravano@gmail.com>
2023-04-21 17:42:03 +0200

    fix navbar mobile

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,86 +1,22 @@
-import * as React from "react";
-import PropTypes from "prop-types";
-import AppBar from "@mui/material/AppBar";
-import Box from "@mui/material/Box";
-import CssBaseline from "@mui/material/CssBaseline";
-import Divider from "@mui/material/Divider";
-import Drawer from "@mui/material/Drawer";
-import IconButton from "@mui/material/IconButton";
-import List from "@mui/material/List";
-import ListItem from "@mui/material/ListItem";
-import ListItemButton from "@mui/material/ListItemButton";
-import ListItemText from "@mui/material/ListItemText";
-import MenuIcon from "@mui/icons-material/Menu";
-import Toolbar from "@mui/material/Toolbar";
-import Typography from "@mui/material/Typography";
-import Button from "@mui/material/Button";
+import { Menu, Search } from "@mui/icons-material";
 import { Link } from "react-router-dom";
-import { Search } from "@mui/icons-material";
 
-const drawerWidth = 240;
-const navItems = ["Home", "About", "Contact"];
-
-const Navbar = (props) => {
-  const { window } = props;
-  const [mobileOpen, setMobileOpen] = React.useState(false);
-
-  const handleDrawerToggle = () => {
-    setMobileOpen((prevState) => !prevState);
-  };
-
-  const drawer = (
-    <Box onClick={handleDrawerToggle} sx={{ textAlign: "center" }}>
-      <Typography variant="h6" sx={{ my: 2 }}>
-        MUI
-      </Typography>
-      <Divider />
-      <List sx={{ display: "block" }}>
-        <Link>Home</Link>
-        <Link>Movies</Link>
-        <Link>TV Shows</Link>
-      </List>
-    </Box>
-  );
-  const container =
-    window !== undefined ? () => window().document.body : undefined;
+const Navbar = () => {
   return (
-    <Box sx={{ display: "flex" }}>
-      <CssBaseline />
-      <AppBar component="nav">
-        <Toolbar className="navbar">
-          <IconButton
-            color="inherit"
-            aria-label="open drawer"
-            edge="start"
-            onClick={handleDrawerToggle}
-            sx={{ mr: 2, display: { sm: "block" } }}
-          >
-            <MenuIcon />
-          </IconButton>
-          <Search />
-        </Toolbar>
-      </AppBar>
-      <Box component="nav">
-        <Drawer
-          container={container}
-          variant="temporary"
-          open={mobileOpen}
-          onClose={handleDrawerToggle}
-          ModalProps={{
-            keepMounted: true, // Better open performance on mobile.
-          }}
-          sx={{
-            display: { xs: "block", sm: "block" },
-            "& .MuiDrawer-paper": {
-              boxSizing: "border-box",
-              width: drawerWidth,
-            },
-          }}
-        >
-          {drawer}
-        </Drawer>
-      </Box>
-    </Box>
+    <div className="navbar_container">
+      <ul className="navbar_links">
+        <li>
+          <Link>
+            <Menu />
+          </Link>
+        </li>
+        <li>
+          <Link>
+            <Search />
+          </Link>
+        </li>
+      </ul>
+    </div>
   );
 };
 

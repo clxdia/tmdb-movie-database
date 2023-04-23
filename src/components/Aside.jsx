ff277b404263a3c0ed83d0c2ac3616bf67672e38
blue cat <01claudia.parravano@gmail.com>
2023-04-23 15:38:55 +0200

    fix ui homepage mb

--- src/components/Aside.jsx
+++ src/components/Aside.jsx
@@ -30,122 +30,117 @@ const Aside = ({ toggleTheme, theme, setTheme }) => {
   };
 
   return (
-    <>
-      <div className="page_wrapper">
-        <div className="aside-home">
-          <ul>
-            <li
-              style={{
-                borderLeftColor: "transparent",
-              }}
+    <div className="page_wrapper">
+      <div className="aside-home">
+        <ul>
+          <li
+            style={{
+              borderLeftColor: "transparent",
+            }}
+          >
+            <Link
+              className="aside-links search_link"
+              onClick={handleOpen}
+              onKeyDown={handleKeyDown}
             >
-              <Link
-                className="aside-links search_link"
-                onClick={handleOpen}
-                onKeyDown={handleKeyDown}
+              <SearchIcon />
+            </Link>
+            {open && (
+              <div
+                className="search_background"
+                onClick={() => setClose(false)}
               >
-                <SearchIcon />
-              </Link>
-              {open && (
-                <div
-                  className="search_background"
-                  onClick={() => setClose(false)}
-                >
-                  <Link className="close_icon" onClick={handleOpen}>
-                    <CloseIcon fontSize="large" />
-                  </Link>
+                <Link className="close_icon" onClick={handleOpen}>
+                  <CloseIcon fontSize="large" />
+                </Link>
 
-                  <div
-                    className="search_bar"
-                    onClick={(e) => e.stopPropagation()}
-                  >
-                    <SearchBar />
-                    <h3>Press ESC to close.</h3>
-                  </div>
+                <div
+                  className="search_bar"
+                  onClick={(e) => e.stopPropagation()}
+                >
+                  <SearchBar />
+                  <h3>Press ESC to close.</h3>
                 </div>
+              </div>
+            )}
+          </li>
+          <li
+            style={{
+              borderLeftColor: active === "Home" ? "#7f5af0" : "transparent",
+              backgroundColor:
+                active === "Home" ? "rgba(199, 199, 199, 0.1)" : "transparent",
+            }}
+          >
+            <Link
+              className="aside-links"
+              to="/"
+              onClick={() => handleLinkClick("Home")}
+              style={{
+                transform: active === "Home" ? "scale(120%)" : "scale(100%)",
+                transition: active === "Home" && "all 300ms ease-in-out",
+              }}
+            >
+              {active === "Home" ? (
+                <AiFillHome size={25} />
+              ) : (
+                <AiOutlineHome size={25} />
               )}
-            </li>
-            <li
+            </Link>
+          </li>
+          <li
+            style={{
+              borderLeftColor: active === "Movies" ? "#7f5af0" : "transparent",
+              backgroundColor:
+                active === "Movies"
+                  ? "rgba(199, 199, 199, 0.1)"
+                  : "transparent",
+            }}
+          >
+            <Link
+              className="aside-links"
+              to="movies"
+              onClick={() => handleLinkClick("Movies")}
               style={{
-                borderLeftColor: active === "Home" ? "#7f5af0" : "transparent",
-                backgroundColor:
-                  active === "Home"
-                    ? "rgba(199, 199, 199, 0.1)"
-                    : "transparent",
+                transform: active === "Movies" ? "scale(120%)" : "scale(100%)",
+                transition: active === "Movies" && "all 300ms ease-in-out",
               }}
             >
-              <Link
-                className="aside-links"
-                to="/"
-                onClick={() => handleLinkClick("Home")}
-                style={{
-                  transform: active === "Home" ? "scale(120%)" : "scale(100%)",
-                  transition: active === "Home" && "all 300ms ease-in-out",
-                }}
-              >
-                {active === "Home" ? (
-                  <AiFillHome size={25} />
-                ) : (
-                  <AiOutlineHome size={25} />
-                )}
-              </Link>
-            </li>
-            <li
+              {active === "Movies" ? (
+                <RiMovie2Fill size={25} />
+              ) : (
+                <RiMovie2Line size={25} />
+              )}
+            </Link>
+          </li>
+          <li
+            style={{
+              borderLeftColor:
+                active === "TV Shows" ? "#7f5af0" : "transparent",
+              backgroundColor:
+                active === "TV Shows"
+                  ? "rgba(199, 199, 199, 0.1)"
+                  : "transparent",
+            }}
+          >
+            <Link
+              className="aside-links"
+              to="tv-shows"
+              onClick={() => handleLinkClick("TV Shows")}
               style={{
-                borderLeftColor:
-                  active === "Movies" ? "#7f5af0" : "transparent",
-                backgroundColor:
-                  active === "Movies"
-                    ? "rgba(199, 199, 199, 0.1)"
-                    : "transparent",
+                transform:
+                  active === "TV Shows" ? "scale(120%)" : "scale(100%)",
+                transition: active === "TV Shows" && "all 300ms ease-in-out",
               }}
             >
-              <Link
-                className="aside-links"
-                to="movies"
-                onClick={() => handleLinkClick("Movies")}
-                style={{
-                  transform:
-                    active === "Movies" ? "scale(120%)" : "scale(100%)",
-                  transition: active === "Movies" && "all 300ms ease-in-out",
-                }}
-              >
-                {active === "Movies" ? (
-                  <RiMovie2Fill size={25} />
-                ) : (
-                  <RiMovie2Line size={25} />
-                )}
-              </Link>
-            </li>
-            <li
-              style={{
-                borderLeftColor:
-                  active === "TV Shows" ? "#7f5af0" : "transparent",
-                backgroundColor:
-                  active === "TV Shows"
-                    ? "rgba(199, 199, 199, 0.1)"
-                    : "transparent",
-              }}
-            >
-              <Link
-                className="aside-links"
-                to="tv-shows"
-                onClick={() => handleLinkClick("TV Shows")}
-                style={{
-                  transform:
-                    active === "TV Shows" ? "scale(120%)" : "scale(100%)",
-                  transition: active === "TV Shows" && "all 300ms ease-in-out",
-                }}
-              >
-                {active === "TV Shows" ? (
-                  <RiTvFill size={25} />
-                ) : (
-                  <RiTvLine size={25} />
-                )}
-              </Link>
-            </li>
-            <div className="switch_container">
-              {/* <ReactSwitch
+              {active === "TV Shows" ? (
+                <RiTvFill size={25} />
+              ) : (
+                <RiTvLine size={25} />
+              )}
+            </Link>
+          </li>
+          <div className="switch_container">
+            {/* <ReactSwitch
               onColor={"#262626"}
               offColor={"#bfbfbf"}
               className="switch"
@@ -154,24 +149,19 @@ const Aside = ({ toggleTheme, theme, setTheme }) => {
               onChange={() => toggleTheme() && setTheme("light")}
               checked={theme === "dark"}
             /> */}
-              <Switch
-                defaultChecked
-                onChange={() => toggleTheme() && setTheme("light")}
-                checked={theme === "dark"}
-              />
-            </div>
-          </ul>
-        </div>
-        {/* <div className="navbar-home">
-          <Navbar toggleTheme={toggleTheme} theme={theme} setTheme={setTheme} />
-        </div> */}
-        <Outlet />
+            <Switch
+              defaultChecked
+              onChange={() => toggleTheme() && setTheme("light")}
+              checked={theme === "dark"}
+            />
+          </div>
+        </ul>
       </div>
       <div className="navbar-home">
         <Navbar toggleTheme={toggleTheme} theme={theme} setTheme={setTheme} />
       </div>
       <Outlet />
-    </>
+    </div>
   );
 };
 

72032775d55b58744280fa35186c4369542813ab
blue cat <01claudia.parravano@gmail.com>
2023-04-27 13:17:20 +0200

    add menu mobile

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,5 +1,14 @@
 import { Close, Menu, Search } from "@mui/icons-material";
-import { getAccordionDetailsUtilityClass, Switch } from "@mui/material";
+import {
+  Divider,
+  getAccordionDetailsUtilityClass,
+  List,
+  ListItem,
+  ListItemText,
+  Paper,
+  Switch,
+} from "@mui/material";
+import { grey } from "@mui/material/colors";
 import { useEffect } from "react";
 import { useState } from "react";
 import { Link } from "react-router-dom";
@@ -12,43 +21,54 @@ const Navbar = ({ toggleTheme, theme, setTheme }) => {
   };
 
   return (
-    <>
-      <div className={`navbar ${open ? "active" : ""}`}>
-        <div className="navbar_container">
-          <ul className="navbar_links">
-            <li onClick={toggleOpen}>
-              <Menu />
+    <div className="navbar_container">
+      <ul className="navbar_links">
+        <li onClick={toggleOpen}>
+          <Menu />
+        </li>
+        <li>
+          <Search />
+        </li>
+      </ul>
+      {open ? (
+        <Paper className="navbar_open" elevation={12}>
+          <ul className="navbar_open_links">
+            <li onClick={() => setOpen(false)}>
+              <Close />
             </li>
             <li>
-              <Search />
+              <Link to="/">Home</Link>
             </li>
+            <li>
+              <Link to="/movies">Movies</Link>
+            </li>
+            <li>
+              <Link to="/tv-shows">TV Shows</Link>
+            </li>
+            <Divider />
           </ul>
-        </div>
-      </div>
-      <div className="navbar_open">
-        <ul className="navbar_open_links">
-          <li onClick={() => setOpen(false)}>
-            <Close />
-          </li>
-          <li>
-            <Link to="/">Home</Link>
-          </li>
-          <li>
-            <Link to="/movies">Movies</Link>
-          </li>
-          <li>
-            <Link to="/tv-shows">TV Shows</Link>
-          </li>
           <div className="switch_container">
+            {/* <ReactSwitch
+              onColor={"#262626"}
+              offColor={"#bfbfbf"}
+              className="switch"
+              uncheckedIcon={<BiMoon className="moon" />}
+              checkedIcon={<BiSun className="sun" />}
+              onChange={() => toggleTheme() && setTheme("light")}
+              checked={theme === "dark"}
+            /> */}
+
             <Switch
               defaultChecked
               onChange={() => toggleTheme() && setTheme("light")}
               checked={theme === "dark"}
             />
           </div>
-        </ul>
-      </div>
-    </>
+        </Paper>
+      ) : (
+        <div className="navbar_close"></div>
+      )}
+    </div>
   );
 };
 
